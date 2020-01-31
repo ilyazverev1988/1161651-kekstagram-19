b08f8d1432f3d8faa71ca069604d16f54f8e7747
ilyazverev1988 <ilya.verez@mail.ru>
2020-01-31 23:18:31 +0300

    31.01.2020_3

--- js/main.js
+++ js/main.js
@@ -1,84 +1,105 @@
 'use strict';
-var countDescribe = 25;
+var NUMBER = 25;
+var messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+var names = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var picture = document.querySelector('.pictures');
 
-var getRandom = function (min, max) {
+var getRandomNumber = function (min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 };
 
-var getMessageFunction = function () {
-  var messageMassive = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  var randomMessage = Math.floor(Math.random() * messageMassive.length);
-  return messageMassive[randomMessage];
+var getRandomElement = function (array) {
+  var random = Math.floor(Math.random() * array.length);
+  return array[random];
 };
 
-var getNameFunction = function () {
-  var nameMassive = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
-  ];
-  var randomName = Math.floor(Math.random() * nameMassive.length);
-  return nameMassive[randomName];
+var getFromArray = function (array) {
+  getRandomElement(array);
 };
 
-var getComments = function (x) {
-  var arrComments = [];
-  for (var i = 0; i < x; i++) {
-    var elementMassiveComment = {
-      avatar: 'photos/' + getRandom(1, 6) + '.jpg',
-      message: getMessageFunction(),
-      name: getNameFunction()
+var getComments = function (number) {
+  var arrayComments = [];
+  for (var i = 0; i < number; i++) {
+    var comment = {
+      avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
+      message: getFromArray(messages),
+      name: getFromArray(names)
     };
-    arrComments.push(elementMassiveComment);
+    arrayComments.push(comment);
   }
-  return arrComments;
+  return arrayComments;
 };
 
-var getMassive = function (x) {
-  var arrUser = [];
-  for (var i = 0; i < x; i++) {
-    var elementMassive = {
-      url: 'photos/' + getRandom(1, 25) + '.jpg',
-      description: 'строка — описание фотографии',
-      likes: getRandom(15, 200),
-      comments: getComments(getRandom(1, 3))
-    };
-    arrUser.push(elementMassive);
+var getRandomNotRepeat = function (min, max) {
+  var totalNumbers = max - min + 1;
+  var arrayTotalNumbers = [];
+  var arrayRandomNumbers = [];
+  var tempRandomNumber;
+
+  while (totalNumbers--) {
+    arrayTotalNumbers.push(totalNumbers + min);
   }
-  return arrUser;
+
+  while (arrayTotalNumbers.length) {
+    tempRandomNumber = Math.round(
+        Math.random() * (arrayTotalNumbers.length - 1)
+    );
+    arrayRandomNumbers.push(arrayTotalNumbers[tempRandomNumber]);
+    arrayTotalNumbers.splice(tempRandomNumber, 1);
+  }
+
+  return arrayRandomNumbers;
 };
 
-var testMassive = getMassive(countDescribe);
-var pictureTemplate = document
-  .querySelector('#picture')
-  .content.querySelector('.picture');
-var picture = document.querySelector('.pictures');
+var getTestData = function (number) {
+  var arrayUsers = [];
+  var arrayRandomNumbers = getRandomNotRepeat(1, number);
+  arrayRandomNumbers.forEach(function (random) {
+    for (var i = 0; i < number; i++) {
+      var elementDataComment = {
+        url: 'photos/' + random + '.jpg',
+        description: 'строка — описание фотографии',
+        likes: getRandomNumber(15, 200),
+        comments: getComments(getRandomNumber(1, 3))
+      };
+    }
+    arrayUsers.push(elementDataComment);
+  });
+  return arrayUsers;
+};
 
-var renderComment = function (x) {
+var testData = getTestData(NUMBER);
+var createComment = function (object) {
+  var pictureTemplate = document
+    .querySelector('#picture')
+    .content.querySelector('.picture');
   var pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = x.url;
-  pictureElement.querySelector('.picture__likes').textContent =
-    x.likes;
+  pictureElement.querySelector('.picture__img').src = object.url;
+  pictureElement.querySelector('.picture__likes').textContent = object.likes;
   pictureElement.querySelector('.picture__comments').textContent =
-    x.comments.length;
+    object.comments.length;
   return pictureElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < countDescribe; i++) {
-  fragment.appendChild(renderComment(testMassive[i]));
-}
+testData.forEach(function (data) {
+  fragment.appendChild(createComment(data));
+});
 
 picture.appendChild(fragment);
