3654511c3110dcd946833957896a72de35f86607
ilyazverev1988 <ilya.verez@mail.ru>
2020-02-08 22:09:32 +0300

    08.02.2020

--- js/form.js
+++ js/form.js
@@ -105,11 +105,175 @@ var checkTags = function (array) {
 
 textHashtag.addEventListener('input', onHashtagCheck);
 
-// ползунок
-var pinSlaider = document.querySelector('.effect-level__pin');
-var lineSlider = document.querySelector('.effect-level__line');
-var inputSlaider = document.querySelector('.effect-level__value');
+// изменение эффекта на картинку
+(function () {
+  var pinSlaider = document.querySelector('.effect-level__pin');
+  var lineSlider = document.querySelector('.effect-level__line');
+  var effectLevel = document.querySelector('.effect-level__depth');
+  var inputSlaider = document.querySelector(
+      '.effect-level__value'
+  );
+  var imagePreview = document.querySelector(
+      '.img-upload__preview'
+  );
+  var effectList = document.querySelector('.effects__list');
+  var slaider = document.querySelector('.effect-level');
 
-pinSlaider.addEventListener('mouseup', function () {
-  inputSlaider.value = (pinSlaider.offsetLeft / lineSlider.offsetWidth) * 100;
-});
+  // функция определения эффекта
+  var addClassEffect = function (evt) {
+    imagePreview.classList = '';
+    imagePreview.style.filter = '';
+    switch (evt.target.id) {
+      case 'effect-none':
+        slaider.classList.add('hidden');
+        break;
+      case 'effect-chrome':
+        slaider.classList.remove('hidden');
+        imagePreview.classList.add('effects__preview--chrome');
+        break;
+      case 'effect-sepia':
+        slaider.classList.remove('hidden');
+        imagePreview.classList.add('effects__preview--sepia');
+        break;
+      case 'effect-marvin':
+        slaider.classList.remove('hidden');
+        imagePreview.classList.add('effects__preview--marvin');
+        break;
+      case 'effect-phobos':
+        slaider.classList.remove('hidden');
+        imagePreview.classList.add('effects__preview--phobos');
+        break;
+      case 'effect-heat':
+        slaider.classList.remove('hidden');
+        imagePreview.classList.add('effects__preview--heat');
+        break;
+      default:
+        slaider.classList.remove('hidden');
+    }
+  };
+
+  // функция расчета эффекта
+  var calculationEffect = function () {
+    switch (imagePreview.className) {
+      case 'effects__preview--chrome':
+        imagePreview.style.filter =
+          'grayscale(' + inputSlaider.value / 100 + ')';
+        break;
+      case 'effects__preview--sepia':
+        imagePreview.style.filter = 'sepia(' + inputSlaider.value / 100 + ')';
+        break;
+      case 'effects__preview--marvin':
+        imagePreview.style.filter = 'invert(' + inputSlaider.value + '%)';
+        break;
+      case 'effects__preview--phobos':
+        imagePreview.style.filter =
+          'blur(' + (inputSlaider.value / 100) * 3 + 'px)';
+        break;
+      case 'effects__preview--heat':
+        imagePreview.style.filter =
+          'brightness(' + (inputSlaider.value / 100) * 3 + ')';
+        break;
+      default:
+        imagePreview.style.filter = '';
+    }
+  };
+
+  // нажатие на ползунок
+  pinSlaider.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX
+      };
+
+      startCoords = {
+        x: moveEvt.clientX
+      };
+
+      var newPosition = pinSlaider.offsetLeft - shift.x;
+      if (newPosition > lineSlider.offsetWidth) {
+        newPosition = lineSlider.offsetWidth;
+      } else if (newPosition < 0) {
+        newPosition = 0;
+      }
+      pinSlaider.style.left = newPosition + 'px';
+      effectLevel.style.width = newPosition + 'px';
+      inputSlaider.value = Math.round(
+          (pinSlaider.offsetLeft / lineSlider.offsetWidth) * 100
+      );
+      calculationEffect();
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  effectList.addEventListener('click', addClassEffect);
+})();
+
+// изменение размеров картинки
+(function () {
+  var controlSmaller = document.querySelector(
+      '.scale__control--smaller'
+  );
+  var controlBigger = document.querySelector(
+      '.scale__control--bigger'
+  );
+  var controlValue = document.querySelector(
+      '.scale__control--value'
+  );
+  var imagePreview = document.querySelector(
+      '.img-upload__preview'
+  );
+  var DEFAULT_VALUE_SIZE = 100;
+  var MIN_VALUE = 0;
+  var STEP_CHANGE_SIZE = 25;
+  var scaleNumber = DEFAULT_VALUE_SIZE;
+  controlValue.value = scaleNumber + '%';
+
+  // функция увеличения размера
+  var onBiggerClick = function () {
+    if (scaleNumber + STEP_CHANGE_SIZE >= DEFAULT_VALUE_SIZE) {
+      scaleNumber = DEFAULT_VALUE_SIZE;
+    } else {
+      scaleNumber += STEP_CHANGE_SIZE;
+    }
+    controlValue.value = scaleNumber + '%';
+    return scaleNumber;
+  };
+  // функция уменьшения размера
+  var onSmallerClick = function () {
+    if (scaleNumber - STEP_CHANGE_SIZE <= MIN_VALUE) {
+      scaleNumber = MIN_VALUE;
+    } else {
+      scaleNumber -= STEP_CHANGE_SIZE;
+    }
+    controlValue.value = scaleNumber + '%';
+    return scaleNumber;
+  };
+
+  controlBigger.addEventListener('click', function () {
+    onBiggerClick();
+    imagePreview.style.transform =
+                   'scale(' + scaleNumber / 100 + ')';
+  });
+  controlSmaller.addEventListener('click', function () {
+    onSmallerClick();
+    imagePreview.style.transform =
+                   'scale(' + scaleNumber / 100 + ')';
+  });
+})();
+
