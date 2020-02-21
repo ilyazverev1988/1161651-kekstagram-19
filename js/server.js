43a571b0435330f438a3d2fcf4d7cbe07cdfcb75
ilyazverev1988 <ilya.verez@mail.ru>
2020-02-21 19:03:46 +0300

    21.02.2020

--- js/server.js
+++ js/server.js
@@ -2,7 +2,6 @@
 
 (function () {
   var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
-  var URL_UPLOAD = 'https://js.dump.academy/kekstagram';
 
   var load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
@@ -44,32 +43,8 @@
     xhr.send();
   };
 
-  var upload = function (data, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Cтатус ответа: : ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-    xhr.open('POST', URL_UPLOAD);
-    xhr.send(data);
-  };
-
   window.server = {
     load: load,
-    upload: upload
   };
 
 })();
